02c3be4d6260ff9f2a1ef9754d9bffe86209ebdb
KrisKotsov <kkotsov@abv.bg>
2024-01-19 06:48:24 +0200

    Create cityRecord.js

---
+++ Objects-and-Composition/cityRecord.js
@@ -0,0 +1,15 @@
+function cityRecord(name, population, treasury) {
+    let city = {
+        name,
+        population,
+        treasury
+    }
+
+    return city
+}
+
+cityRecord('Tortuga',
+
+    7000,
+
+    15000)
